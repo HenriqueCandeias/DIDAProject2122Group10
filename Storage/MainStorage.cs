3e566d39e56e594067180cf902bbbcdfd5de3f83
DannyIsYog <fabiodaniel.2000@gmail.com>
2021-10-22 18:06:08 +0100

    Basic Implementation of DIDAStorageInterface

--- Storage/MainStorage.cs
+++ Storage/MainStorage.cs
@@ -2,11 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DIDAStorage;
 using Grpc.Core;
 using Grpc.Net.Client;
 
 namespace Storage
 {
+    public class StorageImpl : DIDAStorage.IDIDAStorage
+    {
+        public Dictionary<string, DIDARecord> valueStorage;
+
+        public StorageImpl()
+        {
+            valueStorage = new Dictionary<string, DIDARecord>();
+        }
+
+        public DIDARecord read(string id, DIDAVersion version)
+        {
+            return valueStorage[id];
+        }
+
+        public DIDAVersion updateIfValueIs(string id, string oldvalue, string newvalue)
+        {
+            DIDARecord record = valueStorage[id];
+            
+            if (record.val.Equals(oldvalue))
+            {
+                record.val = newvalue;
+                //TODO Add version incrementer
+
+                valueStorage[id] = record;
+            }
+
+            return record.version;
+        }
+
+        public DIDAVersion write(string id, string val)
+        {
+            DIDARecord record = valueStorage[id];
+
+            record.val = val;
+            //TODO Add version incrementer
+
+            valueStorage[id] = record;
+
+            return record.version;
+
+        }
+    }
+
+
     //This is acts as a server
     public class Storage : WorkerService.WorkerServiceBase
     {
